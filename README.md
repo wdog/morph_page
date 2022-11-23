250525f9ee97de00443512fe11d52cc4ab79dec6
wdog <wdog666@gmail.com>
2022-11-23 20:06:47 +0100

    init project morph

---
+++ README.md
@@ -0,0 +1,5 @@
+## MOPRH PAGE
+
+
+
+https://www.youtube.com/watch?v=5RyrIPCs47A&list=PLDyQo7g0_nsXEOxGlAgccV8fu-cHZnI5B&index=2
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // cssnano: {}, // minify using only tailwind not used with vite
+  },
+};
