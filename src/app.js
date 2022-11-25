7d39b81f389a3dd43f03bca8318211518293f4f4
wdog <wdog666@gmail.com>
2022-11-25 12:45:40 +0100

    final test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import "./input.css";
 import gsap from "gsap";
 const button = document.getElementById("open_close");
-var tl = gsap.timeline({ paused: true, reversed: true });
+const tl = gsap.timeline({ paused: true, reversed: true });
 
 tl.add(
+  gsap.to("#info", {
+    height: "100%",
+    ease: "power2.easeOut",
+    duration: 0.5,
+  })
+).add(
   gsap.to(".cover", {
     width: "60%",
-    duration: 0.5,
     ease: "power2.easeOut",
-    onComplete: () => console.log(Date.now()),
-  })
+    duration: 1,
+  }),
+  "-=0.5"
 );
 
 button.addEventListener("click", () => {
   tl.reversed() ? tl.play() : tl.reverse();
 });
+
+
